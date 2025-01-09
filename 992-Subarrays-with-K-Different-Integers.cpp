44808146dbff9cd080209ab5f7e92ea0f11e98c2
Logarathan S V <techdare7@gmail.com>
2025-01-09 22:19:13 +0530

    [992] [Time Beats: 47.35%] [Memory Beats: 28.70%] - LeetPush

--- 992-Subarrays-with-K-Different-Integers.cpp
+++ 992-Subarrays-with-K-Different-Integers.cpp
@@ -3,17 +3,14 @@ public:
     int findarr(vector<int> nums, int k){
         if(k<0) return 0;
         int l = 0;
-        unordered_set<int> st;
         unordered_map<int, int> mp;
         int ct = 0;
         for(int r=0;r<nums.size();r++){
-            st.insert(nums[r]);
             mp[nums[r]]++;
-            while(st.size()>k){
+            while(mp.size()>k){
                 mp[nums[l]]--;
                 if(mp[nums[l]] == 0){
                     mp.erase(nums[l]);
-                    st.erase(nums[l]);
                 }
                 l++;
             }
