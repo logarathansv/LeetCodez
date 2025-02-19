98d2d7f78ebeb9e5c25ba242ca872f48ec53cabd
Logarathan S V <techdare7@gmail.com>
2025-02-19 21:12:44 +0530

    [1415] [Time Beats: 25.43%] [Memory Beats: 77.25%] - LeetPush

--- DCP-02-25/1415-The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
+++ DCP-02-25/1415-The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    void getHappyArr(int n, vector<string>& ans, string str){
+    void getHappyArr(int k, int n, int& inc, string str, string& res){
         if(str.size() == n) {
-            ans.push_back(str);
-            cout<<"ans"<<str;
+            // ans.push_back(str);
+            inc++;
+            if(inc == k) res = str;
             return ;
         }
         cout<<"str"<<str;
         for(char ch='a';ch<='c';ch++){
             // cout<<"ind"<<ind<<" ";
             if(str.length() > 0 && ch == str[str.length()-1]) continue;
-            // str += ch;
-            getHappyArr(n, ans, str+ch);
+            str += ch;
+            getHappyArr(k, n, inc, str, res);
+            if(res != "") return;
+            str.pop_back();
         }
     }
     string getHappyString(int n, int k) {
-        vector<string> ans;
+        // vector<string> ans;
         string str;
+        string res;
+        int inc = 0;
 
-        getHappyArr(n, ans, "");
-        return k-1 >= ans.size() ? "" : ans[k-1];
+        getHappyArr(k, n, inc, "", res);
+        // return k > ans.size() ? "" : ans[k-1];
+        return res;
     }
 };
