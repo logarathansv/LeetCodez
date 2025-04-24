82b091295e0cd8b1829904ddb9ebc82e3d0ddcf6
Logarathan S V <techdare7@gmail.com>
2025-04-24 22:59:45 +0530

    Time: 24 ms (45.78%), Space: 44.1 MB (39.22%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
+    int count(vector<int>& nums, int k){
+        if(k < 0) return 0;
+        unordered_map<int, int> mp;
+
+        int ct = 0, l = 0;
+        for(int r=0;r<nums.size();r++){
+
+            mp[nums[r]]++;
+            
+            while(mp.size() > k){
+                mp[nums[l]]--;
+                if(mp[nums[l]] == 0) mp.erase(nums[l]);
+                l++;
+            }
+            
+            ct += (r - l + 1);
+
+        }
+        cout<<ct;
+        return ct;
+    }   
     int countCompleteSubarrays(vector<int>& nums) {
+        int k = 0;
         unordered_set<int> st;
-        int st1 = 0, ans = 0;
 
         for(auto num:nums) st.insert(num);
 
-        st1 = st.size();
+        k = st.size();
 
-        for(int i=0;i<nums.size();i++){
-            st.clear();
-            for(int j=i;j<nums.size();j++){
-                st.insert(nums[j]);
-                if(st.size() == st1) ans++;
-            }
-        }
-        return ans;
+        return count(nums, k) - count(nums, k-1);
+    
+        // unordered_set<int> st;
+        // int st1 = 0, ans = 0;
+
+        // for(auto num:nums) st.insert(num);
+
+        // st1 = st.size();
+
+        // for(int i=0;i<nums.size();i++){
+        //     st.clear();
+        //     for(int j=i;j<nums.size();j++){
+        //         st.insert(nums[j]);
+        //         if(st.size() == st1) ans++;
+        //     }
+        // }
+        // return ans;
     }
 };
