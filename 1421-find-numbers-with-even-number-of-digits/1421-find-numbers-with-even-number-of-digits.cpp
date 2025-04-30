3b61cdd80a5a3b737159c5e691fcbbce5134e274
Logarathan S V <techdare7@gmail.com>
2025-04-30 15:45:20 +0530

    Time: 3 ms (10.29%), Space: 14.4 MB (11.46%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int ans = 0;
+        for(auto num : nums){
+            if(to_string(num).size() % 2 == 0) ans++;
+        }
+        return ans;
+    }
+};
