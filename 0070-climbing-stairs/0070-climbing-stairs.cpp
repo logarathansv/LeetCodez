5199372a1d21ab25cba7c15e293250442337b860
Logarathan S V <techdare7@gmail.com>
2025-06-16 16:34:09 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int ans = 0;
-    int stepup(int n, int ct, vector<int> &memo){
-        if(ct > n) return 0;
-        if(ct == n) { return 1;}
+    // int stepup(int n, int ct, vector<int> &memo){
+    //     if(ct > n) return 0;
+    //     if(ct == n) { return 1;}
 
-        if(memo[ct] != -1) return memo[ct];
+    //     if(memo[ct] != -1) return memo[ct];
 
-        memo[ct] = stepup(n, ct+1, memo) + stepup(n, ct+2, memo);
-        return memo[ct];
-    }
+    //     memo[ct] = stepup(n, ct+1, memo) + stepup(n, ct+2, memo);
+    //     return memo[ct];
+    // }
     int climbStairs(int n) {
-        vector<int> memo(n+1, -1);
+        vector<int> dp(n+1, -1);
 
-        return stepup(n, 0, memo);
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for(int i=2;i<n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        // return stepup(n, 0, memo);
+        return dp[n-1];
     }
 };
