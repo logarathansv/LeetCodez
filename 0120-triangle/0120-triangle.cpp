5e1aae7e0276216514d5f2ee95b56d21dc01a938
Logarathan S V <techdare7@gmail.com>
2025-07-27 18:29:33 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7.33%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -18,6 +18,18 @@ public:
         int m = triangle[n-1].size();
 
         vector<vector<int>> memo(n, vector<int>(m, -1));
-        return findmin(0, 0, triangle, memo);
+
+        for(int i=0;i<n;i++){
+            memo[n-1][i] = triangle[n-1][i];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                memo[i][j] = min(memo[i+1][j], memo[i+1][j+1]) + triangle[i][j];
+            }
+        }
+
+        return memo[0][0];
+        // return findmin(0, 0, triangle, memo);
     }
 };
