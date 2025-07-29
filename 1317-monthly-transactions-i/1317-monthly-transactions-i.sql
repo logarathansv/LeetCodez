69460ce6d5df91ec6a478f9f0158e5e262f0b31a
Logarathan S V <techdare7@gmail.com>
2025-07-29 15:33:07 +0530

    Time: 672 ms (21.05%), Space: 0B (100%) - LeetHub

---
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -0,0 +1,9 @@
+SELECT DATE_FORMAT(trans_date, '%Y-%m') as month, 
+    country, 
+    COUNT(*) as trans_count, 
+    SUM(IF(state = 'approved', 1, 0)) as approved_count,
+    SUM(amount) as trans_total_amount, 
+    SUM(IF(state = 'approved', amount, 0)) as approved_total_amount 
+
+FROM Transactions
+GROUP BY EXTRACT(YEAR_MONTH FROM trans_date), country;
