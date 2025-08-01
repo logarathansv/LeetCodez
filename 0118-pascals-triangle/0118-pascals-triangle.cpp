b42062558354b98a69e24e3c81fd89e4aa927837
Logarathan S V <techdare7@gmail.com>
2025-08-01 17:56:32 +0530

    Time: 0 ms (100%), Space: 9.9 MB (5.38%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         int n = numRows;
-        vector<vector<int>> dp(numRows+1, vector<int>(numRows+1, 1));
+        vector<vector<int>> dp(numRows+1);
 
-        dp[0][0] = 1;
-        dp[1][0] = 1;
-        dp[1][1] = 1;
+        dp[0].push_back(1);
+        dp[1].push_back(1);
+        dp[1].push_back(1);
 
         for(int i=2;i<n;i++){
+            dp[i].push_back(1);
             for(int j=1;j<i;j++){
-                dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                dp[i].push_back(dp[i-1][j] + dp[i-1][j-1]);
             }
+            dp[i].push_back(1);
         }
 
-        vector<vector<int>> ans;
+        // vector<vector<int>> ans;
 
-        for(int i=0;i<n;i++){
-            vector<int> lev;
-            for(int j=0;j<i+1;j++){
-                lev.push_back(dp[i][j]);
-            }
+        // for(int i=0;i<n;i++){
+        //     vector<int> lev;
+        //     for(int j=0;j<i+1;j++){
+        //         lev.push_back(dp[i][j]);
+        //     }
 
-            ans.push_back(lev);
-        }
+        //     ans.push_back(lev);
+        // }
+        dp.resize(n);
 
-        return ans;
+        return dp;
     }
 };
