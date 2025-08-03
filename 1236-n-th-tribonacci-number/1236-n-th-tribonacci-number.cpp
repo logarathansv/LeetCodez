d0e9874080f2a8c46b9536e7463f0559d6214d57
Logarathan S V <techdare7@gmail.com>
2025-08-03 20:59:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.22%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -3,17 +3,19 @@ public:
     int tribonacci(int n) {
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-
-        vector<int> dp(n+1, -1);
-
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int res = 0;
 
         for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            res = a+b+c;
+            a = b;
+            b = c;
+            c = res;
         }
 
-        return dp[n];
+        return res;
     }
 };
