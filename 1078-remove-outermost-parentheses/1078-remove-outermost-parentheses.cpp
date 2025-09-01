e36c7d3b5be940f05e8f8c7dff20c54789b4a7ce
Logarathan S V <techdare7@gmail.com>
2025-09-01 11:07:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -7,14 +7,12 @@ public:
 
         for(int i=0;i<s.size();i++){
             char ch = s[i];
-            str += ch;
             
             if(ch == '(') ct++;
             else ct--;
 
-            if(ct == 0) {
-                ans += str.substr(1, str.size()-2);
-                str = "";
+            if(ct > 0) {
+                ans += ch;
             }
         }
 
