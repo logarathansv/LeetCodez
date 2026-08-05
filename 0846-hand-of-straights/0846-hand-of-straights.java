269b7d0ab9e98ba3cb9336fd4be5be3f39228fd2
Logarathan S V <techdare7@gmail.com>
2026-08-05 08:02:39 +0530

    Time: 23 ms (92.78%), Space: 47.8 MB (74.12%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -2,25 +2,37 @@ class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if(hand.length % groupSize != 0) return false;
 
-        Map<Integer, Integer> mp = new TreeMap<>();
+        Map<Integer, Integer> mp = new HashMap<>();
 
         for(int i=0;i<hand.length;i++){
             mp.put(hand[i], mp.getOrDefault(hand[i], 0) + 1);
         }
         for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-            while(mp.get(entry.getKey()) > 0){
-                int first = entry.getKey();
-                int s = groupSize;
-                while(s-- != 0 && mp.containsKey(first)){
-                    if(mp.getOrDefault(first, 0) > 0)
-                        mp.put(first, mp.get(first)-1);
-                    else return false;
-                    first += 1;
-                    System.out.print(first);
+            int start = entry.getKey();
+
+            while(mp.getOrDefault(start - 1, 0) > 0) start--;
+            
+            while(mp.getOrDefault(start, 0) > 0){
+
+                for(int i=0;i<groupSize;i++){
+                    int curr = start + i;
+
+                    if(mp.getOrDefault(curr, 0) ==0) return false;
+                    else mp.put(curr, mp.get(curr) - 1);
                 }
-                System.out.println();
-                if(s != -1) return false;
             }
+            // while(mp.get(entry.getKey()) > 0){
+            //     int first = entry.getKey();
+            //     // int s = groupSize;
+            //     for(int i=0;i<groupSize;i++){
+            //         if(mp.getOrDefault(first, 0) > 0)
+            //             mp.put(first, mp.get(first)-1);
+            //         else return false;
+            //         first += 1;
+            //         System.out.print(first);
+            //     }
+            //     System.out.println();
+            // }
         }
 
         return true;
