d46a1f7c2637de8e98b27f3a4e8a06bea2323ae7
Logarathan S V <techdare7@gmail.com>
2026-08-08 10:55:50 +0530

    Time: 0 ms (100%), Space: 20.6 MB (79.21%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-//     int mindist(int ind, vector<int>& nums, vector<int>& dp){
-//         if(ind >= nums.size()-1) return 0;
-
-//         if(dp[ind] != -1) return dp[ind];
-
-//         int mini = INT_MAX;
-//         for(int i=1;i<=nums[ind];i++){
-//             if(ind + i < nums.size()){
-//                 int minidist = mindist(ind + i, nums, dp);
-//                 if(minidist == INT_MAX) continue;
-//                 else minidist += 1; 
-//                 mini = min(mini, minidist);
-//             }
-//         }
-//         return dp[ind] = mini;
-//     }
     int jump(vector<int>& nums) {
-        // vector<int> dp(nums.size(), -1);
-        int l = 0, r = 0, ct = 0;
+        int l = 0, r= 0, jumps = 0;
 
-        while(r<nums.size()-1){
-            int far = 0;
+        while(r < nums.size()-1){
+            int f = 0;
+
             for(int i=l;i<=r;i++){
-                far = max(far, i+nums[i]);
+                f = max(f, nums[i] + i);
             }
-
-            l = r+1;r = far;
-            ct++;
+            
+            l = r+1;
+            r = f;
+            jumps++;
         }
 
-        return ct;
-        // return mindist(0, nums, dp);
+        return jumps;
     }
 };
