c1efa0b1a638b95350eeab97b33cc40e2da3fd6a
Logarathan S V <techdare7@gmail.com>
2026-08-08 11:59:23 +0530

    Time: 0 ms (100%), Space: 18.4 MB (31.06%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
+        queue<pair<int,int>> q;
 
-        int tocolor = image[sr][sc]; 
-
-        int rows[] = {0, -1, 0, 1, 0};
-        int cols[] = {0, 0 ,1, 0, -1};
-
-        queue<pair<int, int>> q;
         q.push({sr, sc});
+        int actual = image[sr][sc];
+        if(color == actual) return image;
+
+        image[sr][sc] = color;
+
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
 
         while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
+            auto front = q.front();
             q.pop();
 
-            for(int i=0;i<5;i++){
-                int nr = r + rows[i];
-                int nc = c + cols[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == tocolor && image[nr][nc] != color){
-                    image[nr][nc] = color;
+            for(int k=0;k<4;k++){
+                int nr = dr[k] + front.first;
+                int nc = dc[k] + front.second;
+                if(nr >=0 && nc >=0 && nr < image.size() && nc <image[0].size() && image[nr][nc] == actual){
                     q.push({nr, nc});
+                    image[nr][nc] = color;
                 }
             }
         }
+
         return image;
     }
 };
