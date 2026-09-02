99703053bc24a736acf1becb97263c3522a4eb3c
Logarathan S V <techdare7@gmail.com>
2026-09-02 08:44:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    // int stepup(int n, int ct, vector<int> &memo){
-    //     if(ct > n) return 0;
-    //     if(ct == n) { return 1;}
+    int find(int ind, int size, vector<int>& dp){
+        if(ind == 0 || ind == 1) return 1;
+        if(dp[ind] != -1) return dp[ind];
 
-    //     if(memo[ct] != -1) return memo[ct];
-
-    //     memo[ct] = stepup(n, ct+1, memo) + stepup(n, ct+2, memo);
-    //     return memo[ct];
-    // }
+        return dp[ind] = find(ind-1, size, dp) + find(ind-2, size, dp); 
+    }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
 
         dp[0] = 1;
-        dp[1] = 2;
+        dp[1] = 1;
 
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=n;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
 
-        // return stepup(n, 0, memo);
-        return dp[n-1];
+        return dp[n];
+        // return find(n, n, dp);
     }
 };
